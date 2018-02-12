7eca7c77e291e388944d80f8f20d19f3986f99d7
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-12 12:52:46 -0800

    Initial client setup complete

---
+++ client/src/components/api_test.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ApiTest extends Component {
+    render(){
+        return (
+            <div>
+                <h1>API testing grounds</h1>
+            </div>
+        )
+    }
+}
+
+export default ApiTest;
